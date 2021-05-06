4da1cad7e2c53d3daefb0c8b1d7d4d07fc3a9e9b
charleneauger <charlene.auger@factorfx.com>
2021-05-06 12:51:15 +0000

    Add css to datatable

---
+++ src/pages/Dashboard/computers-test.js
@@ -0,0 +1,45 @@
+export default {
+    table: [
+        {
+			ID:754,
+            NAME:'D39170',
+            DESCRIPTION:'x86_64/00-00-00 15:02:01',
+            DEVICEID:'D39170-2019-08-01-10-12-22',
+            IPADDR:'10.3.34.21',
+            IPSRC:'10.3.34.21',
+            LASTCOME:'2021-01-28 08:07:02',
+            LASTDATE:'2021-01-28 08:07:02',
+        },
+        {
+            ID:755,
+            NAME:'DC-ULINENETDEV4',
+            DESCRIPTION:'Ulinenet DEV',
+            DEVICEID:'DC-ULINENETDEV4-2019-07-29-09-17-04',
+            IPADDR:'10.50.50.157',
+            IPSRC:'10.50.50.157',
+            LASTCOME:'2021-01-27 11:48:43',
+            LASTDATE:'2021-01-27 11:48:43',
+        },
+        {
+            ID:756,
+            NAME:'DC-CTIDEV',
+            DESCRIPTION:'Computer Telephony Integration Development Server(Callpops)',
+            DEVICEID:'DC-CTIDEV-2019-07-29-09-03-05',
+            IPADDR:'10.35.21.87',
+            IPSRC:'10.35.21.87',
+            LASTCOME:'2021-01-27 18:40:58',
+            LASTDATE:'2021-01-27 18:40:58',
+        },
+        {
+            ID:757,
+            NAME:'D50837',
+            DESCRIPTION:'x86_64/00-00-00 15:01:09',
+            DEVICEID:'D50837-2019-08-01-10-10-40',
+            IPADDR:'10.3.30.16',
+            IPSRC:'10.3.30.16',
+            LASTCOME:'2021-01-28 08:35:32',
+            LASTDATE:'2021-01-28 08:35:32',
+        }
+    ],
+};
+  
