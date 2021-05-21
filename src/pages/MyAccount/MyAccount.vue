543f49cbb2d533cbc1da5bde0ca589f26df893de
charleneauger <charlene.auger@factorfx.com>
2021-05-21 13:55:36 +0000

    Add api patch to myaccount page

--- src/pages/MyAccount/MyAccount.vue
+++ src/pages/MyAccount/MyAccount.vue
@@ -1,59 +1,135 @@
 <template>
-	<div id="MyAccount">
-		<section v-if="errored">
-			<b-alert 
-				:show="!!errorMsg" 
-				class="alert-sm" 
-				variant="danger"
-			>
-				{{ errorMsg }}
-			</b-alert>
-		</section>
-
-		<section v-else>
-			<div v-if="loading">
-				<Loader/>
-			</div>
-
-			<div
-				v-for="row in rowdata" 
-				v-else
-				:key="row.id"
-			>
-				<b-form
-					@submit="onSubmit" 
-					@reset="onReset"
+	<b-container fluid>
+		<div id="MyAccount">
+			
+			<section v-if="successed">
+				<b-alert 
+					:show="!!succesMsg" 
+					class="alert-sm" 
+					variant="success"
 				>
-					<b-form-row>
-						<b-form-group 
-							id="input-group-2" 
-							label="Username :" 
-							label-for="input-2"
-						>
-							<b-form-input
-								id="input-2"
-								v-model="row.username"
-								placeholder="Username"
-								required
-							>{{ row.username }}</b-form-input>
-						</b-form-group>
-						<b-form-group 
-							id="input-group-2" 
-							label="Email :" 
-							label-for="input-2"
-						>
-							<b-form-input
-								id="input-2"
-								v-model="row.email"
-								placeholder="Email"
-								required
-							>{{ row.email }}</b-form-input>
-						</b-form-group>
-					</b-form-row>
-				</b-form>
-			</div>
-		</section>
-	</div>
+					{{ succesMsg }}
+				</b-alert>
+			</section>
+
+			<section v-if="errored">
+				<b-alert 
+					:show="!!errorMsg" 
+					class="alert-sm" 
+					variant="danger"
+				>
+					{{ errorMsg }}
+				</b-alert>
+			</section>
+
+			<section
+				id="formSection"
+			>
+				<div v-if="loading">
+					<Loader/>
+				</div>
+
+				<div
+					v-for="row in rowdata" 
+					v-else
+					:key="row.id"
+					class="account-config"
+				>
+					<b-form
+						@submit="onSubmit"
+					>
+						<b-row>
+							<b-col>
+								<b-form-group
+									label="Username :" 
+									label-for="username"
+								>
+									<b-form-input
+										id="username"
+										v-model="row.username"
+										placeholder="Username"
+										required
+										disabled
+									>{{ row.username }}</b-form-input>
+								</b-form-group>
+							</b-col>
+							<b-col>
+								<b-form-group
+									label="Password :" 
+									label-for="password"
+								>
+									<b-form-input
+										id="password"
+										v-model="row.password"
+										type="password"
+										required
+									/>
+								</b-form-group>
+							</b-col>
+						</b-row>
+						<b-row>
+							<b-col>
+								<b-form-group
+									label="Email :" 
+									label-for="email"
+								>
+									<b-form-input
+										id="email"
+										v-model="row.email"
+										placeholder="Email"
+										required
+									>{{ row.email }}</b-form-input>
+								</b-form-group>
+							</b-col>
+						</b-row>
+						<b-row>
+							<b-col>
+								<b-form-group 
+									label="Firstname :" 
+									label-for="first_name"
+								>
+									<b-form-input
+										id="first_name"
+										v-model="row.first_name"
+										placeholder="Firstname"
+										required
+									>{{ row.first_name }}</b-form-input>
+								</b-form-group>
+							</b-col>
+							<b-col>
+								<b-form-group
+									label="Lastname :" 
+									label-for="last_name"
+								>
+									<b-form-input
+										id="last_name"
+										v-model="row.last_name"
+										placeholder="Lastname"
+										required
+									>{{ row.last_name }}</b-form-input>
+								</b-form-group>
+							</b-col>
+						</b-row>
+						<b-row>
+							<b-col align-self="start"/>
+							<b-col 
+								align-self="center"
+								align="center"
+							>
+								<b-button 
+									type="submit"
+									variant="success"
+								>
+									Save
+								</b-button>
+							</b-col>
+							<b-col align-self="end"/>
+						</b-row>
+					</b-form>
+				</div>
+			</section>
+		</div>
+	</b-container>
 </template>
 
 <script>
@@ -68,9 +144,19 @@ export default {
 	data() {
 		return {
 			errorMsg: null,
-			rowdata: [],
+			succesMsg: null,
+			rowdata: [{
+				password: '',
+			}],
 			loading: true,
 			errored: false,
+			successed: false,
+			password: null,
+		}
+	},
+	watch: {
+		successed: function() {
+			setTimeout(() => this.successed = false, 10000)
 		}
 	},
 	mounted() {
@@ -94,13 +180,38 @@ export default {
 	methods: {
 		onSubmit(event) {
 			event.preventDefault()
-			alert(JSON.stringify(this.rowdata))
-		},
-		onReset(event) {
-			event.preventDefault()
-			// Reset our form values
-			this.rowdata.username = ''
-			this.rowdata.email = ''
+
+			const header = {
+				"Content-Type": "application/json;charset=utf-8",
+				"Authorization": 'Token ' + localStorage.getItem('token_authentication')
+			}
+
+			this.rowdata.forEach(details => {
+
+				var jsonReturn = {
+					"password": details.password,
+					"email": details.email,
+					"first_name": details.first_name,
+					"last_name": details.last_name
+				}
+
+				Axios.patch("http://172.18.26.12:8000/myaccount/"+details.id+"/", jsonReturn, { headers: header })
+					.then(() => {
+						this.succesMsg = "My new account configuration saved !"
+						this.successed = true
+						this.errorMsg = null
+						this.errored = false
+					})
+					.catch(e => {
+						this.errorMsg = e
+						this.errored = true
+						this.succesMsg = null
+						this.successed = false
+					})
+					.finally(() => {
+						this.loading = false
+					})
+			})
 		}
 	}
 }
