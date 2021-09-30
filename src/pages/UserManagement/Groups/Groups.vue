27c2401cedc703c5b0828f60f3d3f4391f8efa1d
charleneauger <charlene.auger@factorfx.com>
2021-09-30 15:50:16 +0000

    Revert call user and group permission

--- src/pages/UserManagement/Groups/Groups.vue
+++ src/pages/UserManagement/Groups/Groups.vue
@@ -18,19 +18,17 @@
 			<div
 				v-else
 			>
-				<AddGroupModal
+				<AddGroupModal 
 					:canadd="canadd"
 					:canedit="canedit"
-					:candelete="candelete"
-				/>
+					:candelete="candelete" />
 			</div>
 		</section>
 	</div>
 </template>
 
 <script>
-import Axios from 'axios';
-import i18n from '../../../i18n';
+import Axios from 'axios'
 import Loader from '@/components/Loader/Loader';
 import AddGroupModal from '@/components/Modals/AddItem/AddGroupModal';
 
@@ -45,11 +43,9 @@ export default {
 			errorMsg: null,
 			loading: true,
 			errored: false,
-			rowdataUserPermissionGroup: [],
-			rowdataUserPermissionUser: [],
-			canadd: false,
-			canedit: false,
-			candelete: false
+			canadd: true,
+			canedit: true,
+			candelete: true
 		}
 	},
 	mounted() {
@@ -58,60 +54,16 @@ export default {
 			"Authorization": 'Token ' + localStorage.getItem('token_authentication')
 		}
 
-		Axios.get(process.env.VUE_APP_API_ROUTE+"myaccount/", { headers: header })
-			.then(response => {
-				// Get permissions
-				response.data.forEach(details => {
-					this.rowdataUserPermissionUser = details.user_permissions
-					details.groups.forEach(groups => {
-						Axios.get(process.env.VUE_APP_API_ROUTE+"groups/"+groups, { headers: header })
-							.then(groupresponse => {
-								groupresponse.data.permissions.forEach(groups => {
-									this.rowdataUserPermissionGroup[groups] = groups
-								})
-								if(this.rowdataUserPermissionUser.indexOf(12) !== -1 
-								|| this.rowdataUserPermissionGroup.indexOf(12) !== -1) {
-									Axios.get(process.env.VUE_APP_API_ROUTE+"groups/", { headers: header })
-										.then(() => {
-											this.errorMsg = null
-											this.errored = false
-											if(this.rowdataUserPermissionUser.indexOf(9) !== -1 
-											|| this.rowdataUserPermissionGroup.indexOf(9) !== -1) {
-												this.canadd = true
-											}
-											if(this.rowdataUserPermissionUser.indexOf(10) !== -1 
-											|| this.rowdataUserPermissionGroup.indexOf(10) !== -1) {
-												this.canedit = true
-											}
-											if(this.rowdataUserPermissionUser.indexOf(11) !== -1 
-											|| this.rowdataUserPermissionGroup.indexOf(11) !== -1) {
-												this.candelete = true
-											}
-										})
-										.catch(e => {
-											this.errorMsg = e
-											this.errored = true
-										})
-										.finally(() => this.loading = false)
-								} else {
-									this.errorMsg = this.errorMsg = i18n.t("dont_have_right_to_see")
-									this.errored = true
-								}
-							})
-							.catch(e => {
-								this.errorMsg = e
-								this.errored = true
-							})
-					})
-					if(details.groups.length == 0) {
-						this.loading = false
-					}
-				})
+		Axios.get(process.env.VUE_APP_API_ROUTE+"groups/", { headers: header })
+			.then(() => {
+				this.errorMsg = null
+				this.errored = false
 			})
 			.catch(e => {
 				this.errorMsg = e
 				this.errored = true
 			})
+			.finally(() => this.loading = false)
 	}
 }
 </script>
--- src/pages/UserManagement/Users/Users.vue
+++ src/pages/UserManagement/Users/Users.vue
@@ -29,8 +29,7 @@
 </template>
 
 <script>
-import Axios from 'axios';
-import i18n from '../../../i18n';
+import Axios from 'axios'
 import Loader from '@/components/Loader/Loader';
 import AddUserModal from '@/components/Modals/AddItem/AddUserModal';
 
@@ -44,13 +43,11 @@ export default {
 		return {
 			errorMsg: null,
 			rowdata: [],
-			rowdataUserPermissionGroup: [],
-			rowdataUserPermissionUser: [],
-			canedit: false,
-			candelete: false,
-			canadd: false,
 			loading: true,
 			errored: false,
+			canadd: true,
+			canedit: true,
+			candelete: true
 		}
 	},
 	mounted() {
@@ -58,57 +55,12 @@ export default {
 			"Content-Type": "application/json;charset=utf-8",
 			"Authorization": 'Token ' + localStorage.getItem('token_authentication')
 		}
-
-		Axios.get(process.env.VUE_APP_API_ROUTE+"myaccount/", { headers: header })
+		
+		Axios.get(process.env.VUE_APP_API_ROUTE+"users/", { headers: header })
 			.then(response => {
-				// Get permissions
-				response.data.forEach(details => {
-					this.rowdataUserPermissionUser = details.user_permissions
-					details.groups.forEach(groups => {
-						Axios.get(process.env.VUE_APP_API_ROUTE+"groups/"+groups, { headers: header })
-							.then(groupresponse => {
-								groupresponse.data.permissions.forEach(groups => {
-									this.rowdataUserPermissionGroup[groups] = groups
-								})
-								if(this.rowdataUserPermissionUser.indexOf(16) !== -1 
-								|| this.rowdataUserPermissionGroup.indexOf(16) !== -1) {
-									Axios.get(process.env.VUE_APP_API_ROUTE+"users/", { headers: header })
-										.then(response => {
-											this.rowdata = response.data
-											this.errorMsg = null
-											this.errored = false
-											if(this.rowdataUserPermissionUser.indexOf(13) !== -1 
-											|| this.rowdataUserPermissionGroup.indexOf(13) !== -1) {
-												this.canadd = true
-											}
-											if(this.rowdataUserPermissionUser.indexOf(14) !== -1 
-											|| this.rowdataUserPermissionGroup.indexOf(14) !== -1) {
-												this.canedit = true
-											}
-											if(this.rowdataUserPermissionUser.indexOf(15) !== -1 
-											|| this.rowdataUserPermissionGroup.indexOf(15) !== -1) {
-												this.candelete = true
-											}
-										})
-										.catch(e => {
-											this.errorMsg = e
-											this.errored = true
-										})
-										.finally(() => this.loading = false)
-								} else {
-									this.errorMsg = this.errorMsg = i18n.t("dont_have_right_to_see")
-									this.errored = true
-								}
-								this.errorMsg = null
-								this.errored = false
-							})
-							.catch(e => {
-								this.errorMsg = e
-								this.errored = true
-							})
-							.finally(() => this.loading = false)
-					})
-				})
+				this.rowdata = response.data
+				this.errorMsg = null
+				this.errored = false
 			})
 			.catch(e => {
 				this.errorMsg = e
