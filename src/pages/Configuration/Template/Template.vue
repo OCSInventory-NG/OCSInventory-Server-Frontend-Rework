1b3b619129cd22c18e3d9dd80619096c4e05f0bc
charleneauger <charlene.auger@factorfx.com>
2021-10-05 09:47:27 +0000

    Add default template for Template page

--- src/pages/Configuration/Template/Template.vue
+++ src/pages/Configuration/Template/Template.vue
@@ -1,9 +1,44 @@
 <template>
-	<div id="TemplatePage"/>
+	<div id="TemplatePage">
+		<section v-if="errored">
+			<b-alert 
+				:show="!!errorMsg" 
+				class="alert-sm" 
+				variant="danger"
+			>
+				{{ errorMsg }}
+			</b-alert>
+		</section>
+
+		<section v-else>
+			<div v-if="loading">
+				<Loader/>
+			</div>
+
+			<div
+				v-else
+			/>
+		</section>
+	</div>
 </template>
 
 <script>
+// eslint-disable-next-line
+import Axios from 'axios'
+import Loader from '@/components/Loader/Loader';
+
 export default {
 	name: 'TemplatePage',
+	components: {
+		Loader
+	},
+	data() {
+		return {
+			errorMsg: null,
+			rowdata: [],
+			loading: true,
+			errored: false
+		}
+	},
 }
 </script>
