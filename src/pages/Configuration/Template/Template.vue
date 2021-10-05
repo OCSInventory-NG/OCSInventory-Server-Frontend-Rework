37618d8068cc85f818499188358cdd0e90d141a3
charleneauger <charlene.auger@factorfx.com>
2021-10-05 16:16:11 +0000

    Add check permissions for template

--- src/pages/Configuration/Template/Template.vue
+++ src/pages/Configuration/Template/Template.vue
@@ -25,7 +25,8 @@
 <script>
 // eslint-disable-next-line
 import Axios from 'axios'
-import Loader from '@/components/Loader/Loader';
+import Loader from '@/components/Loader/Loader'
+import i18n from '../../../i18n'
 
 export default {
 	name: 'TemplatePage',
@@ -37,8 +38,28 @@ export default {
 			errorMsg: null,
 			rowdata: [],
 			loading: true,
-			errored: false
+			errored: false,
+			canadd: false,
+			canedit: false,
+			candelete: false
 		}
 	},
+	mounted() {
+		if(localStorage.getItem('permissions').split(",").includes("view_template")) {
+			if(localStorage.getItem('permissions').split(",").includes("add_template")) {
+				this.canadd = true
+			}
+			if(localStorage.getItem('permissions').split(",").includes("change_template")) {
+				this.canedit = true
+			}
+			if(localStorage.getItem('permissions').split(",").includes("delete_template")) {
+				this.candelete = true
+			}			
+		} else {
+			this.errorMsg = i18n.t("dont_have_right_to_see")
+			this.errored = true
+			this.loading = false
+		}
+	}
 }
 </script>
