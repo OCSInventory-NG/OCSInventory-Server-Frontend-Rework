3d8b442a9a3cd10a41707cbd2c4e82cb6618457b
Gilles DUBOIS <gilles.dubois@factorfx.com>
2022-09-09 17:09:03 +0200

    Add layouts

---
+++ src/layouts/auth.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<div class="container-login">
+			<router-view />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'LoginCOmponent'   // you can enter any name (optional)
+}
+</script>
