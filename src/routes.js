a570df0ba336881a61085ab0ad190dc906a6596f
Gilles DUBOIS <gilles.dubois@factorfx.com>
2022-09-09 17:09:29 +0200

    Rename and update routes

---
+++ src/routes.js
@@ -0,0 +1,89 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+/***** PAGES *****/
+// Login
+import Login from '@/pages/Login/Login'
+// MyAccount
+import MyAccount from '@/pages/MyAccount/MyAccount'
+// Dashboard
+import Dashboard from '@/pages/Dashboard/Dashboard'
+// Inventory
+import Assets from '@/pages/Inventory/Asset/Asset'
+import Ipdiscover from '@/pages/Inventory/Ipdiscover/Ipdiscover'
+// Usermanagement
+import User from '@/pages/UserManagement/User/User'
+import Group from '@/pages/UserManagement/Group/Group'
+// Configuration
+import General from '@/pages/Configuration/General/General'
+import Accountinfo from '@/pages/Configuration/Accountinfo/Accountinfo'
+import Template from '@/pages/Configuration/Template/Template'
+import EditTemplate from '@/pages/Configuration/Template/EditTemplate'
+
+Vue.use(Router)
+
+export default new Router({
+	mode: 'history',
+	base: '/ocsreports',
+	routes: [
+		{
+			path: '/login',
+			name: 'Login',
+			component: Login,
+			meta: {
+				layout: 'auth'
+			}
+		},
+		{
+			path: '/myaccount',
+			name: 'MyAccount',
+			component: MyAccount,
+		},
+		{
+			path: '/dashboard',
+			name: 'Dashboard',
+			component: Dashboard
+		},
+		{
+			path: '/usermanagement/users',
+			name: 'User',
+			component: User
+		},
+		{
+			path: '/usermanagement/groups',
+			name: 'Group',
+			component: Group,
+		},
+		{
+			path: '/configurations/general',
+			name: 'General',
+			component: General,
+		},
+		{
+			path: '/configurations/accountinfo',
+			name: 'Accountinfo',
+			component: Accountinfo
+		},
+		{
+			path: '/configurations/templates',
+			name: 'Template',
+			component: Template
+		},
+		{
+			path: '/configurations/templates/edittemplate/:id',
+			name: 'EditTemplate',
+			component: EditTemplate,
+			props: true
+		},
+		{
+			path: '/inventory/assets',
+			name: 'Assets',
+			component: Assets
+		},
+		{
+			path: '/inventory/ipdiscover',
+			name: 'Ipdiscover',
+			component: Ipdiscover
+		}	
+	],
+})
