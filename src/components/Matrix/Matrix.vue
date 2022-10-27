6c650fa82202e8633944c5ad27b1d23a52103c81
Charlene Auger <charlene.auger@factorfx.com>
2022-10-27 15:17:57 +0000

    Update matrix

--- src/components/Matrix/Matrix.vue
+++ src/components/Matrix/Matrix.vue
@@ -2,7 +2,7 @@
 	<div id="matrix">
 		<table class="table w-full">
 			<thead>
-				<tr>
+				<tr align="center">
 					<th>{{ $t("type") }}</th>
 					<th 
 						v-for="action in headertab"
@@ -14,11 +14,60 @@
 			</thead>
 
 			<tbody>
-				<tr v-for="label in rowlabel" :key="label">
+				<tr 
+					v-for="label in rowlabel" 
+					:key="label.id"
+				>
 					<td>{{ label.trad }}</td>
-					
+					<td
+						v-for="tab in rowtab[label.id]"
+						:key="tab.id"
+						align="center"
+					>
+						<input 
+							:id="tab.name"
+							v-model="selectedPermissionsArray"
+							type="checkbox" 
+							:value="tab.id" 
+						>
+					</td>
+					<td align="center">
+						<input 
+							:id="label.id"
+							type="checkbox" 
+							value="all"
+							@click="selectAllPermission"
+						>
+					</td>
+				</tr>
+				<tr>
+					<td>{{ $t('all') }}</td>
+					<td
+						v-for="action in headertab"
+						:key="action"
+						align="center"
+					>
+						<input 
+							:id="action+'_'"
+							type="checkbox" 
+							value="all"
+							@click="selectAllPermission"
+						>
+					</td>
 				</tr>
 			</tbody>
+
+			<thead>
+				<tr align="center">
+					<th>{{ $t("type") }}</th>
+					<th 
+						v-for="action in headertab"
+						:key="action"
+					>
+						{{ $t(action) }}
+					</th>
+				</tr>
+			</thead>
 		</table>
 	</div>
 </template>
@@ -32,8 +81,57 @@ export default {
 	},
 	data() {
 		return {
-			headertab: ["add", "update", "delete", "view", "all"],
+			headertab: ["add", "change", "delete", "view", "all"],
+			selectedPermissionsArray: []
 		}
-	}
+	},
+	watch: {
+		selectedPermissionsArray: function () {
+			this.$emit('permissions', this.selectedPermissionsArray)
+		}
+	},
+	methods: {
+		selectAllPermission(line) {
+			var checked = line.path[0].checked
+			var id = line.path[0].id
+			var search = null
+			var all = []
+
+			if(id !== "all_") {
+				search = '[id*="'+id+'"]'
+			} else {
+				search = '[id*="add_"],[id*="change_"],[id*="delete_"],[id*="view_"]'
+				all = document.querySelectorAll("input[value=all]");
+			}
+
+			var permissions = document.querySelectorAll(search);
+
+			all.forEach(check => {
+				if(checked) {
+					document.getElementById(check.id).checked = true
+				} else {
+					document.getElementById(check.id).checked = false
+				}
+			})
+
+			permissions.forEach(permission => {
+				var index = this.selectedPermissionsArray.indexOf(parseInt(permission.value));
+
+				if(checked) {
+					document.getElementById(permission.id).checked = true
+					if(permission.value !== "all") {
+						if (index === -1) {
+							this.selectedPermissionsArray.push(parseInt(permission.value))
+						}
+					}
+				} else {
+					document.getElementById(permission.id).checked = false
+					if (index !== -1) {
+						this.selectedPermissionsArray.splice(index, 1);
+					}
+				}
+			})
+		}
+	}	
 }
 </script>
