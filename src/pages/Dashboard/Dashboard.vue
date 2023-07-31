edb3a5deb2d9f9dc23d718cc157adbb9007d94fc
Charlene Auger <charlene.auger@factorfx.com>
2023-07-31 09:42:01 +0000

    Add dashboard assets counter

--- src/pages/Dashboard/Dashboard.vue
+++ src/pages/Dashboard/Dashboard.vue
@@ -3,14 +3,24 @@
 		<PageHeader 
 			page-title="dashboard"
 		/>
+
+		<div class="page-body">
+			<div class="card">
+				<div class="card-body">
+					<AssetCounters />
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 import PageHeader from '@/components/Header/PageHeader'
+//import BarChart from '@/components/Dashboard/Chart/Bar'
+import AssetCounters from '@/components/Dashboard/Counter/AssetCounters'
 
 export default {
 	name: "Dashboard",
-	components: { PageHeader }
+	components: { PageHeader, /*BarChart,*/ AssetCounters }
 }
 </script>
