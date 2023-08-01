44fe5e3ff63338422fda737c57bc3a764011ea2b
Charlene Auger <charlene.auger@factorfx.com>
2023-08-01 08:05:04 +0000

    Add donut chart

---
+++ src/components/Dashboard/Chart/Donut.vue
@@ -0,0 +1,31 @@
+<template>
+	<div id="donut">
+		<div class="col-lg-6">
+			<div
+				id="donut-card" 
+				class="card"
+			>
+				<div class="card-body">
+					<h3 class="card-title">
+						{{ $t(title) }}
+					</h3>
+					<apexchart 
+						:options="options" 
+						:series="series"
+						type="donut" 
+					/>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	name: "Donut",
+	props: {
+		options: { type: Object, default: null },
+		series: { type: Array, default: null },
+		title: { type: String, default: null }
+	}
+}
+</script>
