94d121563f62c460fabd491f766dab1dcb0a2ab9
Charlene Auger <charlene.auger@factorfx.com>
2023-08-01 09:19:06 +0000

    Add line chart

---
+++ src/components/Dashboard/Chart/Line.vue
@@ -0,0 +1,30 @@
+<template>
+	<div id="line">
+		<div
+			id="donut-card" 
+			class="card"
+		>
+			<div class="card-body">
+				<h3 class="card-title">
+					{{ $t(title) }}
+				</h3>
+				<apexchart 
+					type="line" 
+					:options="options" 
+					:series="series"
+				/>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Line",
+	props: {
+		options: { type: Object, default: null },
+		series: { type: Array, default: null },
+		title: { type: String, default: null }
+	}
+}
+</script>
