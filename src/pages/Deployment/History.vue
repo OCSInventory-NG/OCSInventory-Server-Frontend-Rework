eaf45b3a1a4383bedf02768e471a07b4a1dd7b8c
Charlene Auger <charlene.auger@factorfx.com>
2023-10-16 13:21:02 +0000

    Add history deployment page

--- src/pages/Deployment/History.vue
+++ src/pages/Deployment/History.vue
@@ -11,54 +11,101 @@
 			/>
 		</section>
 
-		<!-- Add package history modal -->
+		<!-- Package history -->
 		<section v-else>
 			<div v-if="loading">
 				<Loader />
 			</div>
 
+			<!-- Header page -->
 			<div v-else>
-				bonjour
+				<PageHeader 
+					page-title="history"
+				/>
+				<!-- Display datatable -->
+				<div class="page-body">
+					<div class="card">
+						<div class="card-body">
+							<Datatable
+								id="deployment-history-datatable"
+								:rowdata="rowdata"
+								:rowheader="rowheader"
+								title="history"
+								translationkey="deployment."
+							/>
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
 	</div>
 </template>
 
 <script>
+import Axios from 'axios'
 import i18n from '@/i18n'
 import Loader from '@/components/Loader/Loader'
 import Alert from '@/components/Alert/Alert'
+import Datatable from '@/components/Datatable/Datatable'
+import PageHeader from '@/components/Header/PageHeader'
 
 export default {
 	name: 'History',
-	components: { Loader, Alert },
+	components: { Loader, Alert, Datatable, PageHeader },
 	data() {
 		return {
+			rowdata: [],
+			rowheader: [],
 			errorMsg: null,
 			loading: true,
 			errored: false,
-			canadd: false,
-			canedit: false,
-			candelete: false
+			header: {
+				"Content-Type": "application/json;charset=utf-8",
+				"Authorization": 'Token ' + localStorage.getItem('token_authentication')
+			}
 		}
 	},
 	mounted() {
 		if(localStorage.getItem('permissions').split(",").includes("view_history")) {
-			if(localStorage.getItem('permissions').split(",").includes("add_history")) {
-				this.canadd = true
-			}
-			if(localStorage.getItem('permissions').split(",").includes("change_history")) {
-				this.canedit = true
-			}
-			if(localStorage.getItem('permissions').split(",").includes("delete_history")) {
-				this.candelete = true
-			}
-			this.loading = false
+			this.getHeader()
 		} else {
 			this.errorMsg = i18n.t("message.dont_have_right_to_see")
 			this.errored = true
 			this.loading = false
 		}
+	},
+	methods: {
+		getHeader() {
+			Axios.options(process.env.VUE_APP_API_ROUTE+"deployment/packages/", { headers: this.header })
+				.then(response => {
+					Object.keys(response.data.actions.POST).forEach(field => {
+						this.rowheader.push(field)
+					})
+					this.errorMsg = null
+					this.errored = false
+					this.getPackages()
+				})
+				.catch(e => {
+					this.errorMsg = e.message
+					this.errored = true
+				})
+		},
+		getPackages() {
+			Axios.get(process.env.VUE_APP_API_ROUTE+"deployment/packages/", { headers: this.header })
+				.then(response => {
+					response.data.forEach(packages => {
+						packages.actions_list = packages.actions_list.length
+					})
+					this.rowdata = response.data
+					this.errorMsg = null
+					this.errored = false
+				})
+				.catch(e => {
+					this.errorMsg = e.message
+					this.errored = true
+				})
+				.finally(() => this.loading = false)
+		},
 	}
 }
 </script>
