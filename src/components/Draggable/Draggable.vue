d5f0c1e6957d3e8e676e06f66a278604972de08b
Charlene Auger <charlene.auger@factorfx.com>
2023-11-16 08:48:05 +0000

    Edit draggable component to work with other components

--- src/components/Draggable/Draggable.vue
+++ src/components/Draggable/Draggable.vue
@@ -6,17 +6,21 @@
 		<table class="table table-vcenter table-striped table-hover">
 			<thead>
 				<tr>
+					<th />
 					<th 
 						v-for="theader in rowheader"
 						:key="theader"
 					>
-						{{ $t('deployment.'+theader) }}
+						{{ $t(translationkey+'.'+theader) }}
 					</th>
-					<th>{{ $t('generic.actions') }}</th>
+					<th v-if="canedit || candelete">
+						{{ $t('generic.actions') }}
+					</th>
 				</tr>
 			</thead>
 			<draggable 
 				v-model="rowdatas" 
+				:disabled="!canedit"
 				tag="tbody"
 				@change="updatePriority"
 			>
@@ -24,22 +28,37 @@
 					v-for="value in rowdatas"
 					:key="value.id"
 				>
+					<td>
+						<font-awesome-icon 
+							:icon="['fas', 'bars']"
+							class="draggable-icon"
+						/>
+					</td>
 					<td 
 						v-for="theader in rowheader" 
 						:key="theader"
 					>
 						{{ value[theader] }}
 					</td>
-					<td class="section-table-btn">
+					<td 
+						v-if="candelete || canedit"
+						class="section-table-btn"
+					>
 						<b-button-toolbar>
 							<b-button-group class="mr-1">
-								<EditActionListModal
-									:id="value.id"
+								<!-- Edit button -->
+								<component 
+									:is="editcomponent"
+									v-if="canedit"
+									v-bind="{ id: value.id }"
+									@reloadDatatable="reloadDatatable"
 								/>
 								<delete-item-modal 
+									v-if="candelete"
 									:id="value.id"
 									:name="value.name"
-									parameter="deployment/actions"
+									:parameter="apiroute"
+									@reloadDatatable="reloadDatatable"
 								/>
 							</b-button-group>
 						</b-button-toolbar>
@@ -54,14 +73,25 @@
 import draggable from 'vuedraggable'
 import Axios from 'axios'
 import EditActionListModal from '@/components/Modals/EditItem/EditActionListModal'
+import EditLdapModal from '@/components/Modals/EditItem/EditLdapModal'
 import DeleteItemModal from '@/components/Modals/DeleteItem/DeleteItemModal'
 
 export default {
 	name: "DraggableComponent",
-	components: { draggable, EditActionListModal, DeleteItemModal },
+	components: { 
+		draggable,
+		EditActionListModal,
+		EditLdapModal,
+		DeleteItemModal
+	},
 	props: {
 		rowdata: { type: Array, default: null },
 		rowheader: { type: Array, default: null },
+		translationkey: { type: String, default: "deployment" },
+		apiroute: { type: String, default: "deployment/actions" },
+		editcomponent: { type: String, default: "EditActionListModal" },
+		canedit: { type: Boolean, default: false },
+		candelete: { type: Boolean, default: false }
 	},
 	data() {
 		return {
@@ -90,7 +120,7 @@ export default {
 					priority: priority
 				}
 
-				Axios.patch(process.env.VUE_APP_API_ROUTE+"deployment/actions/"+action.id+"/", json, { headers: this.header })
+				Axios.patch(process.env.VUE_APP_API_ROUTE+this.apiroute+"/"+action.id+"/", json, { headers: this.header })
 					.catch(e => {
 						console.log(e)
 					})
@@ -99,6 +129,9 @@ export default {
 				priority += 1
 			});
 		},
+		reloadDatatable() {
+			this.$emit('reloadDatatable')
+		}
 	}
 }
 </script>
