2476fd41ba2fed113f46cdbeebb0d0526d9a8422
Charlene Auger <charlene.auger@factorfx.com>
2024-03-20 15:59:03 +0100

    Rename file and update for vue3

---
+++ src/route.js
@@ -0,0 +1,241 @@
+import { createWebHistory,createRouter } from "vue-router"
+import AppLayout from "./layouts/AppLayout.vue"
+import AuthLayout from "./layouts/AuthLayout.vue"
+
+const routes = [
+	{
+		path: '/login',
+		name: 'Login',
+		meta: {
+			layout: AuthLayout
+		},
+		component: () => import('@/pages/Login/Login.vue'),
+	},
+	{
+		path: '/myaccount',
+		name: 'MyAccount',
+		component: () => import('@/pages/MyAccount/MyAccount.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/dashboard',
+		name: 'Dashboard',
+		component: () => import('@/pages/Dashboard/Dashboard.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/usermanagement/users',
+		name: 'User',
+		component: () => import('@/pages/UserManagement/User/User.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/usermanagement/groups',
+		name: 'Group',
+		component: () => import('@/pages/UserManagement/Group/Group.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/configurations/general',
+		name: 'General',
+		component: () => import('@/pages/Configuration/General/General.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/configurations/accountinfo',
+		name: 'Accountinfo',
+		component: () => import('@/pages/Configuration/Accountinfo/Accountinfo.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/configurations/templates',
+		name: 'Template',
+		component: () => import('@/pages/Configuration/Template/Template.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/configurations/templates/:id',
+		name: 'EditTemplate',
+		component: () => import('@/pages/Configuration/Template/EditTemplate.vue'),		
+		props: true,
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/configurations/netgroup',
+		name: 'NetworkGroup',
+		component: () => import('@/pages/Configuration/NetworkGroup/NetworkGroup.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/configurations/authentication',
+		name: 'Authentication',
+		component: () => import('@/pages/Configuration/Authentication/Authentication.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/configurations/automaticactions',
+		name: 'AutomaticAction',
+		component: () => import('@/pages/Configuration/AutomaticAction/AutomaticAction.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/configurations/rules',
+		name: 'Rule',
+		component: () => import('@/pages/Configuration/Rule/Rule.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/configurations/rules/:id',
+		name: 'EditRule',
+		component: () => import('@/pages/Configuration/Rule/EditRule.vue'),		
+		props: true,
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/inventory/assets',
+		name: 'Assets',
+		component: () => import('@/pages/Inventory/Asset/Asset.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/inventory/assetgroups',
+		name: 'AssetGroup',
+		component: () => import('@/pages/Inventory/Asset/AssetGroup.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/inventory/assetgroups/:id',
+		name: 'AssetGroupDetail',
+		component: () => import('@/pages/Inventory/Asset/AssetGroupDetail.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/inventory/ipdiscover',
+		name: 'Ipdiscover',
+		component: () => import('@/pages/Inventory/Ipdiscover/Ipdiscover.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/inventory/netdevices/:id?',
+		name: 'Netdevice',
+		component: () => import('@/pages/Inventory/Ipdiscover/Netdevice.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/inventory/inventory_logs/:id?',
+		name: 'InventoryLog',
+		component: () => import('@/pages/Inventory/Tools/InventoryLog.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/inventory/:type/:id',
+		name: 'Detail',
+		component: () => import('@/pages/Detail/Detail.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/inventory/multisearch',
+		name: 'Multisearch',
+		component: () => import('@/pages/Inventory/Tools/Multisearch.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/inventory/savedsearch',
+		name: 'SavedSearch',
+		component: () => import('@/pages/Inventory/Tools/SavedSearch.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/deployment/packages',
+		name: 'Packages',
+		component: () => import('@/pages/Deployment/Package.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/deployment/packages/:id',
+		name: 'EditPackage',
+		component: () => import('@/pages/Deployment/EditPackage.vue'),		
+		props: true,
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/deployment/history',
+		name: 'History',
+		component: () => import('@/pages/Deployment/History.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/deployment/history/:id',
+		name: 'Result',
+		component: () => import('@/pages/Deployment/Result.vue'),	
+		props: true,
+		meta: {
+			layout: AppLayout
+		}
+	},
+	{
+		path: '/deployment/report',
+		name: 'Report',
+		component: () => import('@/pages/Deployment/Report.vue'),
+		meta: {
+			layout: AppLayout
+		}
+	}
+]
+
+const router = createRouter({
+	history: createWebHistory("/ocsreports"),
+	routes,
+})
+  
+export default router
