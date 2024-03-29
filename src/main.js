659bf710f6da21a4e28696eba1f46d00cbb33573
Charlene Auger <charlene.auger@factorfx.com>
2024-03-29 16:07:23 +0000

    Add loader, alert and datatable in main to load in all components

--- src/main.js
+++ src/main.js
@@ -48,6 +48,18 @@ library.add({
 	faBars, faChartSimple, faFileLines, faFloppyDisk, faStar
 })
 
+/***** Loader *****/
+import Loader from '@/components/Loader/Loader.vue'
+app.component("Loader", Loader)
+
+/***** Alert *****/
+import Alert from '@/components/Alert/Alert.vue'
+app.component("Alert", Alert)
+
+/***** Datatable *****/
+import Datatable from '@/components/Datatable/Datatable.vue'
+app.component("Datatable", Datatable)
+
 await router.isReady()
 
 app.mount('#app')
