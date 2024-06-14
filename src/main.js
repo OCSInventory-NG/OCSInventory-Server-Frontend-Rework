16c6f556a6c0bf458536e44ca1ef4695f7a475a8
Charlene Auger <charlene.auger@factorfx.com>
2024-06-14 09:19:38 +0000

    add configuration file

--- src/main.js
+++ src/main.js
@@ -1,33 +1,18 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
-
-const app = createApp(App)
-
 /***** Bootstrap *****/
 import { createBootstrap } from 'bootstrap-vue-next'
-app.use(createBootstrap())
-
 /***** Vue Router *****/
 import router from '@/route'
-app.use(router)
-
 /***** I18n *****/
 import i18n from '@/i18n'
-app.use(i18n)
-
 /***** Apex Charts *****/
 import VueApexCharts from 'vue3-apexcharts'
-app.use(VueApexCharts)
-
 /***** Vue Json Excel 3 *****/
 import JsonExcel from 'vue-json-excel3'
-app.component("DownloadExcel", JsonExcel)
-
 /***** Vue select *****/
 import vSelect from "vue-select"
-app.component("VSelect", vSelect)
 import "vue-select/dist/vue-select.css"
-
 /***** Icons *****/
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon, FontAwesomeLayers } from '@fortawesome/vue-fontawesome'
@@ -39,15 +24,40 @@ import {
 import {
 	faSquare, faSquareCheck, faFileLines, faFloppyDisk, faStar, faWindowMaximize
 } from '@fortawesome/free-regular-svg-icons'
-app.component('FontAwesomeIcon', FontAwesomeIcon)
-app.component('FontAwesomeLayers', FontAwesomeLayers)
-library.add({ 
-	faHome, faCircle, faUsersCog, faAngleRight, faGear, faPowerOff, faUser, faPlus,
-	faMagnifyingGlass, faDownload, faSquare, faSquareCheck, faPencil, faTrashCan,
-	faTriangleExclamation, faXmark, faDesktop, faWrench, faCheck, faUpload, faBoxesPacking,
-	faBars, faChartSimple, faFileLines, faFloppyDisk, faStar, faWindowMaximize
+
+async function loadConfig() {
+	const response = await fetch('/config.json');
+	if (!response.ok) {
+		throw new Error('Failed to load config');
+	}
+	return response.json();
+}
+
+loadConfig().then((config) => {
+	const app = createApp(App)
+
+	app.use(createBootstrap())
+	app.use(router)
+	app.use(i18n)
+	app.use(VueApexCharts)
+	app.component("DownloadExcel", JsonExcel)
+	app.component("VSelect", vSelect)
+	app.component('FontAwesomeIcon', FontAwesomeIcon)
+	app.component('FontAwesomeLayers', FontAwesomeLayers)
+	library.add({ 
+		faHome, faCircle, faUsersCog, faAngleRight, faGear, faPowerOff, faUser, faPlus,
+		faMagnifyingGlass, faDownload, faSquare, faSquareCheck, faPencil, faTrashCan,
+		faTriangleExclamation, faXmark, faDesktop, faWrench, faCheck, faUpload, faBoxesPacking,
+		faBars, faChartSimple, faFileLines, faFloppyDisk, faStar, faWindowMaximize
+	})
+
+	router.isReady()
+
+	console.log(app.config)
+	console.log(config)
+
+	app.config.globalProperties.$config = config;
+	app.mount('#app')
+}).catch((error) => {
+	console.error('Error loading config:', error);
 })
-
-router.isReady()
-
-app.mount('#app')
