e8e376103291c1adcb811852f0cef0d4585ffea0
Charlene Auger <charlene.auger@factorfx.com>
2024-07-29 07:40:18 +0000

    Update dashboard page

--- src/pages/Dashboard/Dashboard.vue
+++ src/pages/Dashboard/Dashboard.vue
@@ -7,108 +7,37 @@
 		<div class="page-body">
 			<div class="card">
 				<div class="card-body">
-					<h1>{{ $t("title.assets") }}</h1>
 					<!-- Error box message -->
-					<section v-if="erroredasset">
+					<section v-if="errored">
 						<Alert 
-							:message="errormsgAsset" 
+							:message="errormsg" 
 							variant="danger"
 						/>
 					</section>
+
+					{{ layouts }}
 					
 					<div 
-						v-if="loadingasset"
+						v-if="loading"
 						class="ocs-loader"
 					>
 						<Loader />
 					</div>
 					<GridLayout 
 						v-else
-						v-model:layout="layout"
+						v-model:layout="layouts.layout"
 						:row-height="30"
 					>
 						<template #item="{ item }">
 							<Counter 
-								v-if="item.i == 0"
-								firsttitle="dashboard.total"
-								:firstcount="total.total"
+								v-if="item.type == 'Counter'"
+								:firsttitle="'dashboard.'+item.chart"
+								:firstcount="chartData[item.chart].total"
 								secondtitle="dashboard.contacted"
-								:secondcount="contacted.total"
-							/>
-							<Counter 
-								v-if="item.i == 1"
-								firsttitle="dashboard.windows"
-								:firstcount="total.windows"
-								secondtitle="dashboard.contacted"
-								:secondcount="contacted.windows"
-							/>
-							<Counter 
-								v-if="item.i == 2"
-								firsttitle="dashboard.linux"
-								:firstcount="total.linux"
-								secondtitle="dashboard.contacted"
-								:secondcount="contacted.linux"
-							/>
-							<Counter 
-								v-if="item.i == 3"
-								firsttitle="dashboard.macos"
-								:firstcount="total.macos"
-								secondtitle="dashboard.contacted"
-								:secondcount="contacted.macos"
-							/>
-							<DonutChart 
-								v-if="item.i == 4"
-								title="dashboard.osassets"
-								:options="oscount.options"
-								:series="oscount.series"
-							/>
-							<LineChart 
-								v-if="item.i == 5"
-								title="dashboard.lastcontacted"
-								:options="lastcontactedopt.options"
-								:series="lastcontactedopt.series"
+								:secondcount="chartData[item.chart].contacted"
 							/>
 						</template>
 					</GridLayout>
-
-					<h1>{{ $t("title.network") }}</h1>
-					<!-- Error box message -->
-					<section v-if="errorednetwork">
-						<Alert 
-							:message="errormsgNetwork" 
-							variant="danger"
-						/>
-					</section>
-					<div 
-						v-if="loadingnetwork"
-						class="ocs-loader"
-					>
-						<Loader />
-					</div>
-
-					<div v-else>
-						<div class="row row-deck row-cards">
-							<Counter 
-								firsttitle="dashboard.totalnetwork"
-								:firstcount="networks.total"
-								classstyle="col-sm-6 col-lg-6"
-							/>
-							<Counter 
-								firsttitle="dashboard.totalnetworkdevices"
-								:firstcount="networks.devices.total"
-								classstyle="col-sm-6 col-lg-6"
-							/>
-						</div>
-						<div class="row">
-							<div class="col-lg-12">
-								<BarChart 
-									title="dashboard.nbdevicebynetwork"
-									:options="networkopt.options"
-									:series="networkopt.series"
-								/>
-							</div>
-						</div>
-					</div>
 				</div>
 			</div>
 		</div>
@@ -133,218 +62,54 @@ export default {
 	data() {
 		return {
 			layout: [
-				{"x":0,"y":0,"w":3,"h":4,"i":"0", static: false},
-				{"x":3,"y":0,"w":3,"h":4,"i":"1", static: false},
-				{"x":6,"y":0,"w":3,"h":4,"i":"2", static: false},
-				{"x":9,"y":0,"w":3,"h":4,"i":"3", static: false},
-				{"x":0,"y":4,"w":6,"h":8,"i":"4", static: false},
-				{"x":6,"y":4,"w":6,"h":8,"i":"5", static: false}
+				{"x":0,"y":0,"w":2,"h":3,"i":"total_ALL","type":"Counter", static: false},
+				{"x":2,"y":0,"w":2,"h":3,"i":"total_WIN","type":"Counter", static: false},
+				{"x":4,"y":0,"w":2,"h":3,"i":"total_LIN","type":"Counter", static: false},
+				{"x":6,"y":0,"w":2,"h":3,"i":"total_MAC","type":"Counter", static: false},
+				{"x":8,"y":0,"w":2,"h":3,"i":"total_LEG","type":"Counter", static: false},
+				{"x":10,"y":0,"w":2,"h":3,"i":"total_SNMP","type":"Counter", static: false}
 			],
+			chartData: [],
 			draggable: true,
 			resizable: true,
-			index: 0,
-			templates: {
-				WIN: [],
-				LIN: [],
-				MAC: [],
-				LEG: [],
-			},
-			total: {
-				total: 0,
-				windows: 0,
-				linux: 0,
-				macos: 0,
-				legacy: 0,
-			},
-			contacted: {
-				total: 0,
-				windows: 0,
-				linux: 0,
-				macos: 0,
-				legacy: 0,
-			},
-			ostemp: {},
-			oscount: {
-				options: {
-					labels: []
-				},
-				series: []
-			},
-			lastcontactedopt: {
-				options: {
-					chart: {
-						id: "last-contacted-chart"
-					},
-					xaxis: {
-						categories: []
-					}
-				},
-				series: [{
-					name: this.$t('dashboard.nbassets'),
-					data: []
-				}]
-			},
-			networks: {
-				total: 0,
-				names: [],
-				devices: {
-					total: 0
-				}
-			},
-			networkopt: {
-				options: {
-					chart: {
-						id: "network-chart"
-					},
-					xaxis: {
-						categories: []
-					}
-				},
-				series: [{
-					name: this.$t('dashboard.nbnetwork'),
-					data: []
-				}]
-			},
-			errormsgAsset: null,
-			errormsgNetwork: null,
-			loadingasset: true,
-			loadingnetwork: true,
-			erroredasset: false,
-			errorednetwork: false,
+			errormsg: null,
+			loading: true,
+			errored: false,
+			layouts: [],
 			header: {
 				"Content-Type": "application/json;charset=utf-8",
 				"Authorization": 'Token ' + localStorage.getItem('token_authentication')
 			}
 		}
 	},
-	mounted() {
-		// Get assets stats
-		this.getTemplates()
-		// Get networks stats
-		this.getNetworks()
+	async beforeMount() {
+		await this.displayDashboard()
+		console.log(this.layouts)
 	},
 	methods: {
-		getNetworks() {
-			axios.get(import.meta.env.VITE_APP_API_ROUTE+"networks/", { headers: this.header })
-				.then(response => {
-					this.networks.total = response.data.length
-					response.data.forEach(network => {
-						this.networks.names[network.id] = {
-							name: network.name,
-							netid: network.netid,
-							nbdevices: 0
-						}
-					})
-					this.errormsgNetwork = null
-					this.errorednetwork = false
-					this.getNetdevices()
-				})
-				.catch(e => {
-					this.errormsgNetwork = e.message
-					this.errorednetwork = true
-				})
+		async displayDashboard() {
+			const response = await axios.get(import.meta.env.VITE_APP_API_ROUTE+"dashboard/layout/", { headers: this.header })
+			this.layouts = response
+			//await this.getChartData()
 		},
-		getNetdevices() {
-			axios.get(import.meta.env.VITE_APP_API_ROUTE+"netdevices/", { headers: this.header })
-				.then(response => {
-					this.networks.devices.total = response.data.length
-					response.data.forEach(device => {
-						this.networks.names[device.network].nbdevices += 1
-					})
-					
-					this.networks.names.forEach(network => {
-						this.networkopt.options.xaxis.categories.push(network.name)
-						this.networkopt.series[0].data.push(network.nbdevices)
-					})
-
-					this.errormsgNetwork = null
-					this.errorednetwork = false
-				})
-				.catch(e => {
-					this.errormsgNetwork = e.message
-					this.errorednetwork = true
-				})
-				.finally(() => this.loadingnetwork = false)
+		getLayouts() {
+			const response = axios.get(import.meta.env.VITE_APP_API_ROUTE+"dashboard/layout/", { headers: this.header })
+			console.log(response)
 		},
-		// Retrieve templates ID and sort by type
-		getTemplates() {
-			axios.get(import.meta.env.VITE_APP_API_ROUTE+"templates/", { headers: this.header })
-				.then(response => {
-					response.data.forEach(template => {
-						if(template.os != "SNMP") {
-							this.templates[template.os].push(template.id)
-						}	
+		getChartData() {
+			for (const chart of this.layouts.layout) {
+				axios.get(import.meta.env.VITE_APP_API_ROUTE+"dashboard/chart/"+chart.i, { headers: this.header })
+					.then(response => {
+						this.loading = true
+						this.chartData[chart.i] = response.data
+						this.errormsg = null
+						this.errored = false
 					})
-					this.errormsgAsset = null
-					this.erroredasset = false
-					this.getCount()
-				})
-				.catch(e => {
-					this.errormsgAsset = e.message
-					this.erroredasset = true
-				})
-		},
-		// Count assets and sort by template types
-		getCount() {
-			axios.get(import.meta.env.VITE_APP_API_ROUTE+"asset/bases/", { headers: this.header })
-				.then(response => {
-					var now = new Date().toJSON().slice(0,10)
-					this.total.total = response.data.length
-					response.data.forEach(asset => {
-						// Counter assets
-						if(asset.last_update.slice(0,10) == now) this.contacted.total += 1
-						if(this.templates.WIN.includes(asset.template)) {
-							this.total.windows += 1
-							if(asset.last_update.slice(0,10) == now) this.contacted.windows += 1
-						}
-						if(this.templates.LIN.includes(asset.template)) {
-							this.total.linux += 1
-							if(asset.last_update.slice(0,10) == now) this.contacted.linux += 1
-						}
-						if(this.templates.MAC.includes(asset.template)) {
-							this.total.macos += 1
-							if(asset.last_update.slice(0,10) == now) this.contacted.macos += 1
-						}
-						if(this.templates.LEG.includes(asset.template)) {
-							this.total.legacy += 1
-							if(asset.last_update.slice(0,10) == now) this.contacted.legacy += 1
-						}
-						// Counter operating system
-						this.ostemp[asset.osname] = (this.ostemp[asset.osname]) ? this.ostemp[asset.osname]+1 : 1
+					.catch(e => {
+						this.errormsg = e.message
+						this.errored = true
 					})
-
-					Object.keys(this.ostemp).forEach(osname => {
-						this.oscount.options.labels.push(osname)
-						this.oscount.series.push(this.ostemp[osname])
-					})
-
-					// Last 7 days contact
-					var dateNow = new Date()
-					var date = new Date()
-					date = date.setDate(dateNow.getDate() - 7)
-					date = new Date(date)
-
-					for(let i = 0; date.toJSON().slice(0,10) != dateNow.toJSON().slice(0,10); i++) {
-						this.lastcontactedopt.options.xaxis.categories.push(date.toJSON().slice(0,10))
-						this.lastcontactedopt.series[0].data[i] = 0
-						date = date.setDate(date.getDate() + 1)
-						date = new Date(date)
-
-						response.data.forEach(asset => {
-							if(asset.last_update.slice(0,10) == date.toJSON().slice(0,10)) {
-								this.lastcontactedopt.series[0].data[i] += 1
-							}
-						})
-					}
-
-					this.errormsgAsset = null
-					this.erroredasset = false
-				})
-				.catch(e => {
-					this.errormsgAsset = e.message
-					this.erroredasset = true
-				})
-				.finally(() => this.loadingasset = false)
+			}
 		}
 	}
 }
