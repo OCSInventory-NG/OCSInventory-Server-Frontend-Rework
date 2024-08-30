1d24109bdd1d8482a0be1d073cfc11f8b0d23b8c
Charlene Auger <charlene.auger@factorfx.com>
2024-08-30 07:53:09 +0000

    Fix linter

--- src/main.js
+++ src/main.js
@@ -49,13 +49,10 @@ loadConfig().then((config) => {
 		faHome, faCircle, faUsersCog, faAngleRight, faGear, faPowerOff, faUser, faPlus,
 		faMagnifyingGlass, faDownload, faSquare, faSquareCheck, faPencil, faTrashCan,
 		faTriangleExclamation, faXmark, faDesktop, faWrench, faCheck, faUpload, faBoxesPacking,
-		faBars, faChartSimple, faFileLines, faFloppyDisk, faStar, faWindowMaximize
+		faBars, faChartSimple, faFileLines, faFloppyDisk, faStar, faWindowMaximize, faSliders
 	})
 
 	router.isReady()
-
-	console.log(app.config)
-	console.log(config)
 
 	app.config.globalProperties.$config = config;
 	app.mount('#app')
