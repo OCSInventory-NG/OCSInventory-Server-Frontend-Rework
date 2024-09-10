fd214d983901db0afcda599816c7017bcf3422c3
Charlene Auger <charlene.auger@factorfx.com>
2024-09-10 15:04:41 +0000

    Update all chart components

--- src/components/Dashboard/Chart/Line.vue
+++ src/components/Dashboard/Chart/Line.vue
@@ -1,14 +1,16 @@
 <template>
 	<div id="line">
 		<div class="card donut-card">
-			<div class="card-body">
+			<div class="card-body line-card">
 				<h3 class="card-title">
 					{{ $t(title) }}
 				</h3>
 				<apexchart 
 					type="line" 
-					:options="options" 
+					:options="chartOptions" 
 					:series="series"
+					height="274.69"
+					width="100%"
 				/>
 			</div>
 		</div>
@@ -22,6 +24,32 @@ export default {
 		options: { type: Object, default: null },
 		series: { type: Array, default: null },
 		title: { type: String, default: null }
+	},
+	data() {
+		return {
+			chartOptions: {
+				chart: {
+					height: 275,
+					type: 'line'
+				},
+				responsive: [
+					{
+						breakpoint: 568,
+						options: {
+							chart: {
+								width: 300
+							},
+							legend: {
+								show: false
+							}
+						}
+					}
+				]
+			}
+		}
+	},
+	async mounted() {
+		this.chartOptions.xaxis = this.options["xaxis"]
 	}
 }
 </script>
