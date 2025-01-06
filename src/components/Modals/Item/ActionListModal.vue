6b9334287613556cf63ef0225cf42bdeaa9bebdf
Antoine ROBIN <antoine.robin@factorfx.com>
2025-01-06 13:24:06 +0000

    fix(ActionListModal): stop sending file if it's not file variable type

--- src/components/Modals/Item/ActionListModal.vue
+++ src/components/Modals/Item/ActionListModal.vue
@@ -1,160 +1,78 @@
 <template>
 	<div id="action-list-modal">
-		<div 
-			v-if="!update"
-			class="page-header d-print-none"
-		>
+		<div v-if="!update" class="page-header d-print-none">
 			<div class="row">
 				<div class="col-auto ms-auto">
-					<b-button
-						:title="$t('deployment.addaction')"
-						variant="primary"
-						class="d-sm-inline-block btn-modal"
-						@click="actionlistmodal = !actionlistmodal"
-					>
-						<font-awesome-icon 
-							:icon="['fas', 'plus']"
-						/>
+					<b-button :title="$t('deployment.addaction')" variant="primary" class="d-sm-inline-block btn-modal"
+						@click="actionlistmodal = !actionlistmodal">
+						<font-awesome-icon :icon="['fas', 'plus']" />
 						{{ $t('deployment.addaction') }}
 					</b-button>
 				</div>
 			</div>
 		</div>
 		<div v-else>
-			<button 
-				:title="$t('deployment.editaction')"
-				class="btn btn-ghost-dark"
-				@click="loadData(id)"
-			>
-				<font-awesome-icon 
-					:icon="['fas', 'pencil']"
-				/>
+			<button :title="$t('deployment.editaction')" class="btn btn-ghost-dark" @click="loadData(id)">
+				<font-awesome-icon :icon="['fas', 'pencil']" />
 			</button>
 		</div>
-		<b-modal 
-			id="actionlistmodal" 
-			v-model="actionlistmodal"
-			:title="(!update) ? $t('deployment.addaction') : $t('deployment.editaction')"
-			hide-footer
-			modal-class="custom-modal modal-blur"
-		>
+		<b-modal id="actionlistmodal" v-model="actionlistmodal"
+			:title="(!update) ? $t('deployment.addaction') : $t('deployment.editaction')" hide-footer
+			modal-class="custom-modal modal-blur">
 			<template #header="{ close }">
 				<h5 class="modal-title">
 					{{ (!update) ? $t('deployment.addaction') : $t('deployment.editaction') }}
-					<b-spinner 
-						v-if="loadingcreate"
-						variant="success"
-					/>
-					<font-awesome-icon 
-						v-if="createwithsuccess"
-						:icon="['fas', 'check']"
-						color="green"
-					/>
-					<font-awesome-icon 
-						v-if="createerror"
-						:icon="['fas', 'xmark']"
-						color="red"
-					/>
+					<b-spinner v-if="loadingcreate" variant="success" />
+					<font-awesome-icon v-if="createwithsuccess" :icon="['fas', 'check']" color="green" />
+					<font-awesome-icon v-if="createerror" :icon="['fas', 'xmark']" color="red" />
 				</h5>
-				<b-button 
-					size="sm" 
-					variant="outline-danger" 
-					@click="close()"
-				>
-					<font-awesome-icon 
-						:icon="['fas', 'xmark']"
-						size="1x"
-					/>
+				<b-button size="sm" variant="outline-danger" @click="close()">
+					<font-awesome-icon :icon="['fas', 'xmark']" size="1x" />
 				</b-button>
 			</template>
-			<Alert 
-				v-if="createerror || errored"
-				:message="(createerror) ? createerrormsg : errormsg" 
-				variant="danger"
-			/>
-			<b-form
-				v-if="!loading"
-				@submit="onSubmit"
-			>
+			<Alert v-if="createerror || errored" :message="(createerror) ? createerrormsg : errormsg"
+				variant="danger" />
+			<b-form v-if="!loading" @submit="onSubmit">
 				<b-row>
 					<b-col>
-						<b-form-group
-							:label="$t('deployment.name')" 
-							label-for="name"
-						>
-							<b-form-input
-								id="name"
-								v-model="row.name"
-								required
-							/>
+						<b-form-group :label="$t('deployment.name')" label-for="name">
+							<b-form-input id="name" v-model="row.name" required />
 						</b-form-group>
 					</b-col>
 				</b-row>
 				<b-row>
 					<b-col>
-						<b-form-group
-							:label="$t('deployment.action_type')" 
-							label-for="action_type"
-						>
-							<v-select
-								id="action_type"
-								v-model="row.action_type" 
-								:options="actionoptions" 
-								:reduce="text => text.value"
-								:clearable="false"
-								label="text"
-								class="mb-3"
-							/>
+						<b-form-group :label="$t('deployment.action_type')" label-for="action_type">
+							<v-select id="action_type" v-model="row.action_type" :options="actionoptions"
+								:reduce="text => text.value" :clearable="false" label="text" class="mb-3" />
 						</b-form-group>
 					</b-col>
 				</b-row>
 				<b-row>
 					<b-col>
-						<b-form-group
-							:label="row.action_type == 'STORE' ? 
-								$t('deployment.path') : $t('deployment.command')" 
-							label-for="command"
-						>
-							<b-form-input
-								id="command"
-								v-model="row.command"
-								required
-							/>
+						<b-form-group :label="row.action_type == 'STORE' ?
+							$t('deployment.path') : $t('deployment.command')" label-for="command">
+							<b-form-input id="command" v-model="row.command" required />
 						</b-form-group>
 					</b-col>
 				</b-row>
-				<b-row
-					v-if="row.action_type == 'STORE' || row.action_type == 'LAUNCH'"
-				>
+				<b-row v-if="row.action_type == 'STORE' || row.action_type == 'LAUNCH'">
 					<b-col>
-						<b-form-file
-							id="file"
-							:placeholder="row.action_type == 'LAUNCH' ? 
-								$t('deployment.select_launch_file') : $t('deployment.select_store_file')"
-							@change="processFile($event)"
-						/>
+						<b-form-file id="file" :placeholder="row.action_type == 'LAUNCH' ?
+							$t('deployment.select_launch_file') : $t('deployment.select_store_file')" @change="processFile($event)" />
 					</b-col>
 				</b-row>
 				<b-row>
 					<b-col align-self="start" />
-					<b-col 
-						align-self="center"
-						align="center"
-					>
-						<b-button 
-							type="submit"
-							variant="success"
-						>
+					<b-col align-self="center" align="center">
+						<b-button type="submit" variant="success">
 							{{ (!update) ? $t('generic.add') : $t('generic.save') }}
 						</b-button>
 					</b-col>
 					<b-col align-self="end" />
 				</b-row>
 			</b-form>
-			<div 
-				v-if="loading"
-				class="ocs-loader"
-			>
+			<div v-if="loading" class="ocs-loader">
 				<Loader />
 			</div>
 		</b-modal>
@@ -201,7 +119,7 @@ export default {
 		}
 	},
 	watch: {
-		createwithsuccess: function() {
+		createwithsuccess: function () {
 			setTimeout(() => {
 				this.actionlistmodal = false
 				this.createwithsuccess = false
@@ -213,7 +131,7 @@ export default {
 					command: null,
 					original_file_name: null
 				}
-				if(this.update) {
+				if (this.update) {
 					this.$emit('reloadDatatable')
 				} else {
 					this.$emit('reloadPackage')
@@ -222,7 +140,7 @@ export default {
 		}
 	},
 	mounted() {
-		if(!this.update) {
+		if (!this.update) {
 			this.row.package = this.package
 			this.loading = false
 		}
@@ -234,7 +152,7 @@ export default {
 			this.getAction(id)
 		},
 		async getAction(id) {
-			await axios.get(this.$config.BACKEND_API_ROUTE+"deployment/actions/"+id, { headers: this.header })
+			await axios.get(this.$config.BACKEND_API_ROUTE + "deployment/actions/" + id, { headers: this.header })
 				.then(response => {
 					this.row = response.data
 					this.errormsg = null
@@ -246,14 +164,14 @@ export default {
 				})
 				.finally(() => this.loading = false)
 		},
-		processFile(event){
+		processFile(event) {
 			this.row.file = event.target.files[0];
 		},
 		onSubmit(event) {
 			event.preventDefault()
 			this.loadingcreate = true
 
-			if(!this.row.file) {
+			if (!this.row.file) {
 				delete this.row.file
 				this.row.original_file_name = null
 			} else {
@@ -263,11 +181,14 @@ export default {
 			let formdata = new FormData()
 
 			Object.keys(this.row).forEach(key => {
+				if (!(this.row["file"] instanceof File)) {
+					delete this.row["file"]
+				}
 				formdata.append(key, this.row[key])
 			})
-			
-			if(!this.update) {
-				axios.post(this.$config.BACKEND_API_ROUTE+"deployment/actions/", formdata, { headers: this.header })
+
+			if (!this.update) {
+				axios.post(this.$config.BACKEND_API_ROUTE + "deployment/actions/", formdata, { headers: this.header })
 					.then(() => {
 						this.createwithsuccess = true
 						this.createerrormsg = null
@@ -280,7 +201,7 @@ export default {
 					})
 					.finally(() => this.loadingcreate = false)
 			} else {
-				axios.patch(this.$config.BACKEND_API_ROUTE+"deployment/actions/"+this.row.id+"/", formdata,
+				axios.patch(this.$config.BACKEND_API_ROUTE + "deployment/actions/" + this.row.id + "/", formdata,
 					{ headers: this.header })
 					.then(() => {
 						this.createwithsuccess = true
@@ -293,7 +214,7 @@ export default {
 						this.createwithsuccess = false
 					})
 					.finally(() => this.loadingcreate = false)
-			}			
+			}
 		}
 	}
 }
