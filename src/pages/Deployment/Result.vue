d2981c082accbf473fc59130f5cda16f1c799160
Antoine ROBIN <antoine.robin@factorfx.com>
2025-03-12 08:46:48 +0000

    fix(package history): linter fixes

--- src/pages/Deployment/Result.vue
+++ src/pages/Deployment/Result.vue
@@ -212,7 +212,13 @@ export default {
 				})
 		},
 		async getPackageResult() {
-			await axios.get(this.$config.BACKEND_API_ROUTE+"deployment/results/?package="+this.id+"&expand=asset,group", { headers: this.header })
+			await axios.get(
+					this.$config.BACKEND_API_ROUTE+
+					"deployment/results/?package="+
+					this.id+
+					"&expand=asset,group",
+					{ headers: this.header }
+				)
 				.then(response => {
 					this.rowdata = response.data
 
