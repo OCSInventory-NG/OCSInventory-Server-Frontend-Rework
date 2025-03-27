70b51679ebdc8da222f78b6047cd280cc12295ef
Charlene Auger <charlene.auger@factorfx.com>
2025-03-27 13:05:19 +0000

    refactor(template): update edit template page to work with expandableFields server branch

--- src/pages/Configuration/Template/EditTemplate.vue
+++ src/pages/Configuration/Template/EditTemplate.vue
@@ -92,7 +92,7 @@ export default {
 	},
 	methods: {
 		async getTemplate() {
-			await axios.get(this.$config.BACKEND_API_ROUTE+"templates/"+this.id, { headers: this.header })
+			await axios.get(this.$config.BACKEND_API_ROUTE+"templates/"+this.id+"?expand=*", { headers: this.header })
 				.then(response => {
 					this.rowtemplatedata = response.data
 					this.rowsectiondata = response.data.sections
