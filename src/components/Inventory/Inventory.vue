3b2e6c38d0ddf30d24ad443ec64152480d3e5fdb
Charlene Auger <charlene.auger@factorfx.com>
2025-04-04 15:16:05 +0000

    fix(linter): fix linter errors

--- src/components/Inventory/Inventory.vue
+++ src/components/Inventory/Inventory.vue
@@ -36,8 +36,8 @@
 export default {
 	name: "Inventory",
 	props: {
-		section: { type: Object, default: null },
-		inventory: { type: Array, default: null }
+		section: { type: Object, default: () => {} },
+		inventory: { type: Array, default: () => [] }
 	},
 	data() {
 		return {
