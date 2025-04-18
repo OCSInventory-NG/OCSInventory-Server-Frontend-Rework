07c03ab73a0084006a1cdb61decef098d344ac88
Charlene Auger <charlene.auger@factorfx.com>
2025-04-18 14:30:43 +0000

    refactor(field): remove is_protected from rowheader

--- src/pages/Configuration/Category/Category.vue
+++ src/pages/Configuration/Category/Category.vue
@@ -100,7 +100,7 @@ export default {
 			await axios.options(this.$config.BACKEND_API_ROUTE+"categories/", { headers: this.header })
 				.then(response => {
 					Object.keys(response.data.actions.POST).forEach(field => {
-						if (field != "inventory_sections") {
+						if (!["inventory_sections", "is_protected"].includes(field) ) {
 							this.rowheader.push(field)
 						}
 					})
