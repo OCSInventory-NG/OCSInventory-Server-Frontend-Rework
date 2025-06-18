0dd705980d0e9888de4df3fe50478270bb680c71
Charlene Auger <charlene.auger@factorfx.com>
2025-06-18 07:11:19 +0000

    refactor(snmp): add autocomplete off

--- src/components/Modals/Item/SnmpModal.vue
+++ src/components/Modals/Item/SnmpModal.vue
@@ -115,6 +115,7 @@
 							<b-form-input
 								id="user"
 								v-model="row.user"
+								autocomplete="off"
 							/>
 						</b-form-group>
 					</b-col>
@@ -144,6 +145,7 @@
 								id="password"
 								v-model="row.password"
 								type="password"
+								autocomplete="off"
 							/>
 						</b-form-group>
 					</b-col>
@@ -159,6 +161,7 @@
 								v-model="row.auth_protocol" 
 								:options="apoptions" 
 								class="mb-3 form-select"
+								autocomplete="off"
 							/>
 						</b-form-group>
 					</b-col>
@@ -174,6 +177,7 @@
 								v-model="row.priv_protocol" 
 								:options="ppoptions" 
 								class="mb-3 form-select"
+								autocomplete="off"
 							/>
 						</b-form-group>
 					</b-col>
@@ -188,6 +192,7 @@
 								id="priv_password"
 								v-model="row.priv_password"
 								type="password"
+								autocomplete="off"
 							/>
 						</b-form-group>
 					</b-col>
