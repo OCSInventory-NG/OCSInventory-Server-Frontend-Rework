c02ba6e8ee3f03209cc13b26104d121ac1913a2b
Charlene Auger <charlene.auger@factorfx.com>
2025-11-13 08:21:54 +0000

    refactor(automation): filter is_protected filed

--- src/pages/Configuration/Automation/Automation.vue
+++ src/pages/Configuration/Automation/Automation.vue
@@ -108,7 +108,9 @@ export default {
 			await axios.options(this.$config.BACKEND_API_ROUTE+"automation/scheduler/", { headers: this.header })
 				.then(response => {
 					Object.keys(response.data.actions.POST).forEach(field => {
-						this.rowheader.push(field)
+						if (!["is_protected"].includes(field) ) {
+							this.rowheader.push(field)
+						}
 					})
 					this.errormsg = null
 					this.errored = false
