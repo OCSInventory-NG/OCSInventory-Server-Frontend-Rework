48167fe2b8cd9aa5cc8b79eb3950cc77c54cfb08
Charlene Auger <charlene.auger@factorfx.com>
2025-11-14 14:57:03 +0000

    fix(linter): fix linter error

--- src/components/Alert/Alert.vue
+++ src/components/Alert/Alert.vue
@@ -60,7 +60,10 @@
 							>
 								<path d="M12 9v4" />
 								<path
-									d="M10.363 3.591l-8.106 13.534a1.914 1.914 0 0 0 1.636 2.871h16.214a1.914 1.914 0 0 0 1.636 -2.87l-8.106 -13.536a1.914 1.914 0 0 0 -3.274 0z" />
+									d="M10.363 3.591l-8.106 13.534a1.914 1.914 0 0 0 
+									1.636 2.871h16.214a1.914 1.914 0 0 0 1.636 
+									-2.87l-8.106 -13.536a1.914 1.914 0 0 0 -3.274 0z"
+								/>
 								<path d="M12 16h.01" />
 							</svg>
 							<svg
@@ -124,7 +127,7 @@ export default {
 	props: {
 		message: { type: String, default: null },
 		variant: { type: String, default: 'info'},
-		cols: { type: Boolean, default: true }
+		cols: { type: Boolean, default: false }
 	},
 	data() {
 		return {
