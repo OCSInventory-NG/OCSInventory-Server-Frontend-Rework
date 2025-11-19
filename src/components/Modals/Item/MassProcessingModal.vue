707d4470853d5d6e05697b488ae986eb47926a32
Charlene Auger <charlene.auger@factorfx.com>
2025-11-19 13:36:52 +0000

    feat(massprocessing): add mass processing component

---
+++ src/components/Modals/Item/MassProcessingModal.vue
@@ -0,0 +1,103 @@
+<template>
+	<div id="mass-processing-modal">
+		<div 
+			class="col-auto ms-auto"
+		>
+			<button 
+				:title="$t('accountinfo.massprocessing')"
+				class="form-control btn datatable-btn"
+				@click="(items.length > 0) ? massprocessingmodal = true : emptyselection = !emptyselection"
+			>
+				<font-awesome-icon 
+					:icon="['fas', 'tag']"
+				/>
+			</button>
+		</div>
+		<b-modal 
+			v-model="massprocessingmodal"
+			:title="$t('accountinfo.massprocessing')"
+			hide-footer
+			modal-class="custom-modal modal-blur"
+			size="xl"
+		>
+			<template #header="{ close }">
+				<h5 class="modal-title">
+					{{ $t('accountinfo.massprocessing') }}
+				</h5>
+			</template>
+			<Accountinfo
+				:canedit="true"
+				:massprocessing="true"
+				:items="items"
+				type="ASSET"
+				@reloadDatatable="reloadDatatable"
+			/>
+		</b-modal>
+		<b-modal 
+			v-model="emptyselection"
+			:title="$t('accountinfo.massprocessing')"
+			hide-footer
+			modal-class="custom-modal modal-blur"
+			size="lg"
+		>
+			<template #header="{ close }">
+				<h5 class="modal-title">
+					{{ $t('accountinfo.massprocessing') }}
+				</h5>
+				<b-button 
+					size="sm" 
+					variant="outline-danger" 
+					@click="close()"
+				>
+					<font-awesome-icon 
+						:icon="['fas', 'xmark']"
+						size="1x"
+					/>
+				</b-button>
+			</template>
+			<div>
+				<Alert 
+					:message="$t('message.no_selection')" 
+					variant="warning"
+				/>
+			</div>
+		</b-modal>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "MassProcessingModal",
+	props: {
+		items: { type: Array, default: () => [] }
+	},
+	data() {
+		return {
+			emptyselection: false,
+			massprocessingmodal: false,
+			errored: false,
+			errormsg: null,
+			loadingcreate: false,
+			successed: false,
+			header: {
+				"Content-Type": "application/json;charset=utf-8",
+				"Authorization": 'Token ' + localStorage.getItem('token_authentication')
+			}
+		}
+	},
+	watch: {
+		successed: function() {
+			setTimeout(() => {
+				this.massprocessingmodal = false
+				this.successed = false
+				this.$emit('reloadDatatable')
+			}, 100)
+		}
+	},
+	methods: {
+		reloadDatatable() {
+			this.successed = true
+		} 
+	}
+}
+</script>
