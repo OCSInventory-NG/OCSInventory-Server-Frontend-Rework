d4fa6fd0cc9fe46b187709e3b272868204b9d9b1
hugotheault <hugo.theault.1@gmail.com>
2026-02-02 16:34:08 +0000

    fix(dates): fix date for netdevices and networks

--- src/pages/Detail/Detail.vue
+++ src/pages/Detail/Detail.vue
@@ -171,7 +171,7 @@
 													{{ $t(translationkey+key) }}
 												</div>
 												<div class="datagrid-content">
-													{{ value }}
+													{{ formatDate(value, key) }}
 												</div>
 											</div>
 										</div>
@@ -338,7 +338,7 @@ export default {
 			});
 		},
 		formatDate(value, key) {
-			const dateFields = ['last_update', 'last_updated', 'timestamp', 'date_created']
+			const dateFields = ['last_update', 'last_updated', 'timestamp', 'date_created', 'last_seen']
 			if (this.$te('inventory.' + value)) return this.$t('inventory.' + value)
 			if (dateFields.includes(key)) {
 				return new Date(value).toLocaleString(this.$i18n.locale)
