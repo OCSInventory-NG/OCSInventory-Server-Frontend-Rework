b16bd567f6a1488e6c5858840652e0197a4bd94d
Charlene Auger <charlene.auger@factorfx.com>
2026-02-04 10:19:15 +0000

    feat(extension)Ã: add extension modal

--- src/components/global.js
+++ src/components/global.js
@@ -24,6 +24,7 @@ import SnmpModal from '@/components/Modals/Item/SnmpModal.vue'
 import SnmpScannerModal from '@/components/Modals/Item/SnmpScannerModal.vue'
 import UserModal from '@/components/Modals/Item/UserModal.vue'
 import Draggable from '@/components/Draggable/Draggable.vue'
+import ExtensionModal from '@/components/Modals/Item/ExtensionModal.vue'
 
 const GlobalComponents = {
 	install(Vue) {
@@ -53,6 +54,7 @@ const GlobalComponents = {
 		Vue.component('SnmpModal', SnmpModal)
 		Vue.component('SnmpScannerModal', SnmpScannerModal)
 		Vue.component('UserModal', UserModal)
+		Vue.component('ExtensionModal', ExtensionModal)
 	}
 }
 
