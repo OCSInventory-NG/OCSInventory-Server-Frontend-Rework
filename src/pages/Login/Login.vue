a330e75489582dc9e043a2ac0f555a1013210a2b
Charlene Auger <charlene.auger@factorfx.com>
2026-02-12 15:16:51 +0000

    refactor(login): improve api calls on login page

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -59,10 +59,10 @@
 								type="submit"
 								class="auth-btn mb-3"
 								variant="inverse"
-								:disabled="loadingLogin"
+								:disabled="loading"
 							>
 								<b-spinner
-									v-if="loadingLogin"
+									v-if="loading"
 									small
 									class="me-2"
 								/>
@@ -87,103 +87,96 @@
 </template>
 
 <script>
-import axios from 'axios'
-
 export default {
 	name: "Login",
 	data() {
 		return {
 			errormsg: null,
 			errored: false,
+
 			sso: false,
 			redirect_url: null,
 			username: null,
 			password: null,
-			loadingLogin: false,
 			langs: [
-				{value: 'fr', text: 'Français'},
-				{value: 'en', text: 'English'},
-			]
-		};
-	},
-	beforeCreate() {
-		const header = {
-			"Content-Type": "application/json;charset=utf-8"
+				{ value: 'fr', text: 'Français' },
+				{ value: 'en', text: 'English' },
+			],
+			
+			loading: false,
 		}
-		axios.get(this.$config.BACKEND_API_ROUTE+"login/", { headers: header })
-			.then(response => {
-				if(response.data) {
+	},
+
+	async beforeCreate() {
+		try {
+			const data = await this.$api.generic.get("login/")
+
+			this.errored = false
+			this.errormsg = null
+			this.sso = !!data?.SSO
+			this.redirect_url = data?.redirect_url ?? null
+		} catch (e) {
+			this.errored = true
+			this.errormsg = (e.response?.data?.error) ? e.response.data.error : e.message
+		}
+	},
+
+	methods: {
+		async onSubmit(event) {
+			event.preventDefault()
+			this.loading = true
+
+			try {
+				const loginOptions = {
+					username: this.username,
+					password: this.password,
+				}
+
+				// NOTE: pas de headers ici (gérés par ton instance axios)
+				const data = await this.$api.generic.post("api-auth/token", loginOptions)
+
+				this.errored = false
+				this.errormsg = null
+
+				localStorage.setItem('token_authentication', data.token)
+				localStorage.setItem('authenticated', true)
+				localStorage.setItem('locale', this.$root.$i18n.locale)
+
+				await this.getPermissions()
+			} catch (e) {
+				this.errored = true
+
+				if (e?.response?.data?.non_field_errors?.length) {
+					this.errormsg = e.response.data.non_field_errors[0]
+				} else {
+					this.errormsg = (e.response?.data?.error) ? e.response.data.error : e.message
+				}
+			} finally {
+				this.loading = false
+			}
+		},
+
+		async getPermissions() {
+			try {
+				// Si ton $api ajoute automatiquement Authorization depuis localStorage,
+				// ça suffit. Sinon, il faudra le gérer via un interceptor.
+				const account = await this.$api.generic.get("myaccount/")
+
+				const perms = account?.full_permissions || []
+				if (perms.length) {
 					this.errored = false
 					this.errormsg = null
-					this.sso = response.data.SSO
-					this.redirect_url = response.data.redirect_url
+					localStorage.setItem('permissions', perms)
+					this.$router.push('/dashboard')
+				} else {
+					this.errored = true
+					this.errormsg = this.$t("message.error_no_permissions")
 				}
-			})
-			.catch(e => {
+			} catch (e) {
 				this.errored = true
 				this.errormsg = (e.response?.data?.error) ? e.response.data.error : e.message
-			})
-	},
-	methods: {
-		onSubmit(event) {
-			event.preventDefault()
-			this.loadingLogin = true
-			const loginOptions = { 
-				"username": this.username,
-				"password": this.password
 			}
-
-			const header = {
-				"Access-Control-Allow-Origin" : "*",
-				"Access-Control-Allow-Methods" : "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-				"Content-Type": "application/json;charset=utf-8"
-			}
-
-			axios.post(this.$config.BACKEND_API_ROUTE+"api-auth/token", loginOptions, { header })
-				.then(response => {
-					this.errored = false
-					this.errormsg = null
-					localStorage.setItem('token_authentication', response.data.token)
-					localStorage.setItem('authenticated', true)
-					localStorage.setItem('locale', this.$root.$i18n.locale)
-					this.getPermissions()
-				})
-				.catch(e => {
-					this.errored = true
-					if(e.response.data.non_field_errors) {
-						this.errormsg = e.response.data.non_field_errors[0]
-					} else {
-						this.errormsg = (e.response?.data?.error) ? e.response.data.error : e.message
-					}
-				})
-				.finally(() => {
-					this.loadingLogin = false
-				})
 		},
-		getPermissions() {
-			const header = {
-				"Content-Type": "application/json;charset=utf-8",
-				"Authorization": 'Token ' + localStorage.getItem('token_authentication')
-			}
-
-			axios.get(this.$config.BACKEND_API_ROUTE+"myaccount/", { headers: header })
-				.then(responseAccount => {
-					var tmpUser = responseAccount.data.full_permissions
-					if(tmpUser.length != 0) {
-						this.errored = false
-						this.errormsg = null
-						localStorage.setItem('permissions', tmpUser)
-						this.$router.push('/dashboard')
-					} else {
-						this.errored = true
-						this.errormsg = this.$t("message.error_no_permissions")
-					}
-				})
-				.catch(e => {
-					this.errored = true
-					this.errormsg = (e.response?.data?.error) ? e.response.data.error : e.message
-				})
-		}
-	}
+	},
 }
 </script>
