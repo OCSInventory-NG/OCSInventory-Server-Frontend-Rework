f951cda2ae50a51201e455a0709c0eab5e74fed5
Charlene Auger <charlene.auger@factorfx.com>
2026-02-16 10:35:16 +0000

    fix(global): fix duplicate component name

--- src/components/global.js
+++ src/components/global.js
@@ -70,7 +70,7 @@ const GlobalComponents = {
 		Vue.component('LineChart', LineChart)
 		Vue.component('ActionListModal', ActionListModal)
 		Vue.component('LdapModal', LdapModal)
-		Vue.component('Draggable', draggable)
+		Vue.component('draggable', draggable)
 	}
 }
 
