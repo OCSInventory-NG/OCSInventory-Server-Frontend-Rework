a71270b7c27bae5809ef6ff0a8cbd138f29c5842
noepoirier <noepoirier@users.noreply.github.com>
2026-03-05 15:21:52 +0000

    [MegaLinter] Apply linters fixes

--- src/pages/NotFound/NotFound.vue
+++ src/pages/NotFound/NotFound.vue
@@ -4,14 +4,17 @@
 		class="container-xl"
 	>
 		<div>
-
 			<!-- Display 404 error -->
 			<div class="page-body">
 				<div class="card">
 					<div class="card-body">
 						<div class="text-center">
-							<h1 class="display-1 fw-bold">404</h1>
-							<h2 class="mt-3">{{ $t('generic.not_found') }}</h2>
+							<h1 class="display-1 fw-bold">
+								404
+							</h1>
+							<h2 class="mt-3">
+								{{ $t('generic.not_found') }}
+							</h2>
 						</div>
 					</div>
 				</div>
