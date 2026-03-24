7862b319f376f05b38c0cdc8c4f0d605c8416e29
faupointalexandre <alexandre.faupoint@factorfx.com>
2026-03-24 14:32:07 +0000

    fix(multisearch): add checkbox ungrouped search

--- src/components/Filter/Search.vue
+++ src/components/Filter/Search.vue
@@ -251,6 +251,7 @@
 					align="center"
 					class="multisearch-btns"
 				>
+				<div class="mb-2">
 					<b-button 
 						type="button"
 						variant="info"
@@ -265,6 +266,13 @@
 					>
 						{{ $t('generic.search') }}
 					</b-button>
+				</div>
+				<b-form-checkbox
+					v-model="ungroup"
+					inline
+				>
+					{{ $t('search.ungroup') }}
+				</b-form-checkbox>
 				</b-col>
 				<b-col align-self="end" />
 			</b-row>
@@ -282,6 +290,7 @@ export default {
 	data() {
 		return {
 			errored: false,
+			ungroup: false,
 
 			errormsg: null,
 			successmsg: null,
@@ -510,7 +519,10 @@ export default {
 		onSubmit(event) {
 			event.preventDefault()
 			localStorage.setItem("multisearch", JSON.stringify(this.datavalues))
-			this.$emit("reloadDatatable", this.datavalues)
+			this.$emit("reloadDatatable", {
+				search_data: this.datavalues,
+				ungroup: this.ungroup
+			})
 		},
 
 		async getFields(route, masterindex, index, section = false, loadingdata = false) {
