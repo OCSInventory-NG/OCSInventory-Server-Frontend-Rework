91802dd36ef333266de530346eabc2290629db11
Noé Poirier <97550546+noepoirier@users.noreply.github.com>
2026-03-26 16:22:56 +0100

    fix(myaccount): harmonize required fields based on users page

--- src/pages/MyAccount/MyAccount.vue
+++ src/pages/MyAccount/MyAccount.vue
@@ -77,7 +77,6 @@
 											<b-form-input
 												id="email"
 												v-model="rowdata.email"
-												required
 											>
 												{{ rowdata.email }}
 											</b-form-input>
@@ -94,7 +93,6 @@
 											<b-form-input
 												id="first_name"
 												v-model="rowdata.first_name"
-												required
 											>
 												{{ rowdata.first_name }}
 											</b-form-input>
@@ -109,7 +107,6 @@
 											<b-form-input
 												id="last_name"
 												v-model="rowdata.last_name"
-												required
 											>
 												{{ rowdata.last_name }}
 											</b-form-input>
