2654c151d54451e7d525c26941c2a6871ffa7e23
Noé Poirier <97550546+noepoirier@users.noreply.github.com>
2026-03-26 16:24:28 +0100

    fix: display email format errors

--- src/pages/MyAccount/MyAccount.vue
+++ src/pages/MyAccount/MyAccount.vue
@@ -199,8 +199,8 @@ export default {
 				this.errormsg = null
 				this.errored = false
 			} catch (e) {
-				this.errormsg = (e.response?.data?.error)
-					? e.response.data.error
+				this.errormsg = (e.response?.data?.error || e.response?.data?.email)
+					? e.response.data.error || e.response.data.email
 					: e.message
 				this.errored = true
 				this.successmsg = null
