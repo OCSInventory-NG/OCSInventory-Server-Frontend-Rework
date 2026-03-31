2601e9b5068eeec22bdef1f90b6765f6c69233a5
Gilles DUBOIS <gilles.dubois@factorfx.com>
2026-03-31 16:21:44 +0200

    fix(eslint): fix eslint config mks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,56 +1,31 @@
-import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
 import vue from "eslint-plugin-vue";
 import globals from "globals";
-import parser from "vue-eslint-parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default defineConfig([{
-    extends: compat.extends("eslint:recommended", "plugin:vue/recommended"),
-
-    plugins: {
-        vue,
-    },
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-        },
-
-        parser: parser,
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-
-    rules: {
-        indent: ["error", "tab"],
-
-        "max-len": ["error", {
-            code: 128,
-        }],
-
-        "no-tabs": 0,
-
-        "no-unused-vars": ["error", {
-            varsIgnorePattern: "^_",
-        }],
-
-        "vue/html-indent": ["error", "tab"],
-        "vue/multi-word-component-names": "off",
-        "vue/no-reserved-component-names": "off",
-        "vue/valid-v-slot": "error",
-        "vue/no-multiple-template-root": "off",
-        "vue/no-v-model-argument": "off",
-    },
-}]);
+export default [
+	js.configs.recommended,
+	...vue.configs["flat/recommended"],
+	{
+		files: ["**/*.{js,mjs,cjs,vue}"],
+		languageOptions: {
+			ecmaVersion: "latest",
+			sourceType: "module",
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		},
+		rules: {
+			indent: ["error", "tab"],
+			"max-len": ["error", { code: 128 }],
+			"no-tabs": "off",
+			"no-unused-vars": ["error", { varsIgnorePattern: "^_" }],
+			"vue/html-indent": ["error", "tab"],
+			"vue/multi-word-component-names": "off",
+			"vue/no-reserved-component-names": "off",
+			"vue/valid-v-slot": "error",
+			"vue/no-multiple-template-root": "off",
+			"vue/no-v-model-argument": "off"
+		}
+	}
+];
