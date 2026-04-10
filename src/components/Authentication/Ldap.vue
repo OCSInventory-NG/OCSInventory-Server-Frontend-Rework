e76b0c56e77bd004c946b0a4bc034fdadf3a0d6e
Charlene Auger <charlene.auger@factorfx.com>
2026-04-10 07:54:00 +0000

    fix(ldap): remove sticky on ldap datatable

--- src/components/Authentication/Ldap.vue
+++ src/components/Authentication/Ldap.vue
@@ -74,7 +74,6 @@
 					:canedit="canedit && !viewOnly"
 					:candelete="candelete"
 					:canaddmapping="canaddmapping"
-					is-sticky
 					editcomponent="LdapModal"
 					translationkey=""
 					apiroute="auth_config"
