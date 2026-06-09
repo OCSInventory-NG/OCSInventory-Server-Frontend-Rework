e25b2fd6c36912beba5e1d3194e567db27c05a54
faupointalexandre <alexandre.faupoint@factorfx.com>
2026-06-09 09:02:00 +0000

    fix(Draggable): fix aggressive refresh reorder

--- src/components/Draggable/Draggable.vue
+++ src/components/Draggable/Draggable.vue
@@ -145,10 +145,12 @@ export default {
 			event?.preventDefault?.()
 
 			try {
+				this.rowdatas.forEach((item, idx) => {
+					item[this.field] = idx + 1
+				})
+
 				const idx = event?.newIndex
 				if (idx == null || !this.rowdatas?.[idx]) return
-
-				this.rowdatas[idx][this.field] = idx + 1
 
 				const payload = { ...this.rowdatas[idx] }
 				if (payload.file) delete payload.file
@@ -157,8 +159,6 @@ export default {
 					`${this.apiroute}/${payload.id}/`,
 					payload
 				)
-
-				this.$emit("reloadDatatable")
 			} catch (e) {
 				console.log(e)
 			}
