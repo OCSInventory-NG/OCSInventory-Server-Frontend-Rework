d736c0a0cbf61c5025964a31d7aef378f040e3c0
hugotheault <hugo.theault.1@gmail.com>
2026-06-10 08:41:27 +0000

    fix(main): fix extension page reload issue

--- src/main.js
+++ src/main.js
@@ -121,7 +121,7 @@ loadConfig().then(async (config) => {
 			)
 
 			// Re-resolve route after dynamic addRoute()
-			if (to.matched.length === 0) {
+			if (to.matched.length === 0 || to.name === 'NotFound') {
 				return to.fullPath
 			}
 		} catch (e) {
