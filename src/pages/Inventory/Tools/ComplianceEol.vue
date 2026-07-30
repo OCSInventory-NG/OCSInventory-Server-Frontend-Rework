04a452cce67baa0a3524ddaa15789cebadbe228d
faupointalexandre <alexandre.faupoint@factorfx.com>
2026-07-30 07:57:05 +0000

    chore(compliance): remove ComplianceEol and ComplianceResults replaced by dashboard

--- src/pages/Inventory/Tools/ComplianceEol.vue
+++
@@ -1,3 +0,0 @@
-<!-- Replaced by ComplianceDashboard.vue (tab "Fin de vie des systèmes") -->
-<template><div /></template>
-<script>export default { name: 'ComplianceEol' }</script>
--- src/pages/Inventory/Tools/ComplianceResults.vue
+++
@@ -1,3 +0,0 @@
-<!-- Replaced by ComplianceDashboard.vue (tab "Résultats de conformité") -->
-<template><div /></template>
-<script>export default { name: 'ComplianceResults' }</script>
